69d20a02cfd64e549a367f21d3c187d13802406d
Mai Thành Duy An <tiachop0102@gmail.com>
2025-05-16 06:25:49 -0400

    Update file permissions for various configuration and script files; enhance Docker setup for Odoo and Traefik services.

--- services/odoo/install.sh
+++ services/odoo/install.sh
@@ -9,15 +9,19 @@ echo "Odoo GID: $GROUP_INFO"
 
 # Gán quyền cho các thư mục mount từ host
 sudo chown -R $USER_INFO:$GROUP_INFO ./odoo/web-data ./odoo/log ./odoo/addons ./odoo/conf
-sudo chmod -R 700 ./odoo/web-data ./odoo/log ./odoo/addons ./odoo/conf
+sudo chmod -R 755 ./odoo/web-data ./odoo/log ./odoo/addons ./odoo/conf
 
 echo "Đã cập nhật quyền cho các thư mục Odoo."
 
 # chown -R 100:101 web-data log addons conf
 # chown -R 1000:1000 web-data log addons conf
-# chmod -R 700 web-data log addons conf
+# chmod -R 755 web-data log addons conf
 # Chạy lệnh sau để khởi động lại container Odoo
 # docker-compose up -d
 # Hoặc nếu bạn muốn chạy lệnh này trong một script, bạn có thể sử dụng:
 # docker-compose restart odoo_18
 # sudo chmod -R 775 
+#  docker compose -f '/home/soloscale/services/odoo/docker-compose.yml' down 
+#  docker compose -f 'soloscale/services/odoo/docker-compose.yml' up -d --build
+# docker compose -f 'soloscale/services/odoo/docker-compose.yml' restart odoo_18
+# docker compose -f 'soloscale/services/odoo/docker-compose.yml' restart 
